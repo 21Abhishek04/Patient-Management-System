b4479e6acf7f39c6c45f68c039fb7fc17072f368
21Abhishek04 <abhishekgrkh@gmail.com>
2022-03-04 17:42:18 +0530

    new

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BusinessLayer_PMS;
+using BusinessObject_PMS;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,30 @@ namespace Patient_Management_System
     {
         static void Main(string[] args)
         {
+            Patients bo = new Patients();
+            Console.WriteLine("Enter Patient info as Id,PatientName,Birthdate,Gender,Weight,BloodGroupId,PhoneNumber,DepartmentId,AppointmentDate,BloodGroup,DeptName");
+            bo._Id = int.Parse(Console.ReadLine());
+            //bo._PatientName = Console.ReadLine();
+            //bo._Birthdate = DateTime.Parse(Console.ReadLine());
+            //bo._Gender = char.Parse(Console.ReadLine());
+            //bo._Weight = float.Parse(Console.ReadLine());
+            //bo._BloodGroupId = int.Parse(Console.ReadLine());
+            //bo._PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            //bo._DepartmentId = int.Parse(Console.ReadLine());
+            //bo._AppointmentDate = DateTime.Parse(Console.ReadLine());
+
+
+            BLPMS pbl = new BLPMS();
+            int h = pbl.SaveDetails(bo);
+            if (h != 0)
+                Console.WriteLine("Successfully");
+            else
+                Console.WriteLine("Failed");
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
