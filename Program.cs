59beaddf61614a02f119bb44551e3c037e7529a1
21Abhishek04 <abhishekgrkh@gmail.com>
2022-03-08 09:43:00 +0530

    NewforAll

--- Program.cs
+++ Program.cs
@@ -15,31 +15,51 @@ namespace Patient_Management_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press 1 to Add Patient\nPress 2 to Delete Patients Info\nPress 3 to Get All Patients info\n Press 4 to get All Details By Patients Id\nPress 5 to Get All Details By Department Names\nPress 6 to Update Details");
+
+            ForDoctor();
+            
+
+           
+        }
+        
+        static void ForDoctor()
+        {
+            Console.WriteLine("Press 1 to Add Patient info\nPress 2 to Delete Patients Info\nPress 3 to Get All Patients info\nPress 4 to get All Details By Patients Id\nPress 5 to Get All Details By Department Names\nPress 6 to Update Details");
             int a = int.Parse(Console.ReadLine());
             switch (a)
             {
-               
-                case 1:GetAdded();
-                    break;
-                case 2: SaveDeleted();
-                    break;
-                case 3:Console.WriteLine();
+
+                case 1:
+                    GetAdded(); Console.WriteLine("Press Enter to Exit");
                     break;
 
-                case 4:  GetAllDetailsbyid();
+                case 2:
+                    SaveDeleted(); Console.WriteLine("Press Enter to Exit");
+                    break;
+
+                case 3:
+                    GetAllPatientsinfo();
+                    Console.WriteLine("Press Enter to Exit");
+                    break;
+
+                case 4:
+                    GetAllDetailsbyid(); Console.WriteLine("Press Enter to Exit");
                     break;
                 case 5:
-                    Console.WriteLine("WIP");
+                    GetAllPatientsinfobyDepartmentName(); Console.WriteLine("Press Enter to Exit");
                     break;
+
                 case 6:
-                    SaveUpdated();
+                    SaveUpdated(); Console.WriteLine("Press Enter to Exit");
                     break;
+                default:
+                    Console.WriteLine("Enter Correct Input");
+                    break;
+
 
             }
             Console.ReadLine();
         }
-
         static void GetAllDetailsbyid()
         {
 
@@ -51,8 +71,10 @@ namespace Patient_Management_System
 
             foreach (DataRow dr in ds.Tables[0].Rows)
             {
-                Console.WriteLine("ID =" + dr[0] + "\nPatient Name=" + dr[1] + "\nBirthdate=" + dr[2] + "\nGender =" + dr[3] + "\nWeight=" + dr[4] + "\nBloodGroupId=" + dr[5] + "\nPhoneNumber =" + dr[6] + "\nDepartmentId=" + dr[7] + "\nAppointmentDate=" + dr[8]+ "\nDepartment Name =" + dr[10] + "\nBlood Gropu=" + dr[12]);
+                Console.WriteLine("ID = " + dr[0] + "\nPatient Name =" + dr[1] + "\nBirthdate = " + dr[2] + "\nGender = " + dr[3] + "\nWeight = " + dr[4] + "\nBloodGroupId = " + dr[5] + "\nPhoneNumber = "+dr[6] + "\nDepartmentId = "+dr[7] + "\nAppointmentDate = " + dr[8]+ "\nDepartment Name = "+ dr[10] + "\nBlood Group= "+ dr[12]+"\n");
+                
             }
+
 
         }
        
@@ -67,18 +89,16 @@ namespace Patient_Management_System
             if (h != 0)
                 Console.WriteLine(" Deleted Successfully");
             else
+            {
                 Console.WriteLine("Failed");
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
 
         static void SaveUpdated()
         {
-           
-
             PMS_BL bl = new PMS_BL();
-
             Console.WriteLine("Enter Id");
             int c = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Weight");
@@ -89,17 +109,16 @@ namespace Patient_Management_System
             int f = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Appointment Date");
             DateTime g= DateTime.Parse(Console.ReadLine());
-           
-
 
             int h =bl.UpdatedDetails(c,d,e,f,g);
 
             if (h != 0)
                 Console.WriteLine(" Updated Successfully");
             else
+            {
                 Console.WriteLine("Failed");
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
 
@@ -107,16 +126,24 @@ namespace Patient_Management_System
         { 
             PMS_BL bl = new PMS_BL();
             Patients bo = new Patients();
-            Console.WriteLine("Enter Patient info as Id,PatientName,Birthdate,Gender,Weight,BloodGroupId,PhoneNumber,DepartmentId,AppointmentDate,BloodGroup,DeptName");
-             bo._Id = int.Parse(Console.ReadLine());
-             bo._PatientName = Console.ReadLine();
+            Console.WriteLine("Enter Patient info as Id,PatientName,Birthdate,Gender,Weight,BloodGroupId,PhoneNumber,DepartmentId,AppointmentDate");
+            Console.WriteLine("Enter ID");
+            bo._Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter PatientName ");
+            bo._PatientName = Console.ReadLine();
+            Console.WriteLine("Enter Birthdate");
             bo._Birthdate = DateTime.Parse(Console.ReadLine());
-            
+            Console.WriteLine("Enter Gender");
             bo._Gender = char.Parse(Console.ReadLine());
+            Console.WriteLine("Weight");
             bo._Weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter BloodGroupId");
             bo._BloodGroupId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
             bo._PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter DepartmentId");
             bo._DepartmentId = int.Parse(Console.ReadLine());
+            Console.WriteLine("AppointmentDate");
             bo._AppointmentDate = DateTime.Parse(Console.ReadLine());
 
            
@@ -124,11 +151,43 @@ namespace Patient_Management_System
             if (h != 0)
                 Console.WriteLine("Added Successfully");
             else
+            {
                 Console.WriteLine("Failed");
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
+        static void GetAllPatientsinfobyDepartmentName()
+        {
+
+            PMS_BL bl = new PMS_BL();
+            Patients bo = new Patients();
+            Console.WriteLine("Enter Department Name");
+            bo._DepartmentName = Console.ReadLine();
+
+            DataSet ds = bl.GetAllPatientsinfobyDepartmentName(bo._DepartmentName);
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                Console.WriteLine("ID =" + dr[0] + "\nPatient Name=" + dr[1] + "\nBirthdate=" + dr[2] + "\nGender =" + dr[3] + "\nWeight=" + dr[4] + "\nBloodGroupId=" + dr[5] + "\nPhoneNumber =" + dr[6] + "\nDepartmentId=" + dr[7] + "\nAppointmentDate=" + dr[8] + "\nDepartment Name =" + dr[10] + "\nBlood Gropu=" + dr[12]);
+            }
+
+        }
+
+        static void GetAllPatientsinfo()
+        {
+
+            PMS_BL bl = new PMS_BL();
+            
+           
+            DataSet ds = bl.GetAllPatientsinfo();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                Console.WriteLine("ID =" + dr[0] + "\nPatient Name=" + dr[1] + "\nBirthdate=" + dr[2] + "\nGender =" + dr[3] + "\nWeight=" + dr[4] + "\nBloodGroupId=" + dr[5] + "\nPhoneNumber =" + dr[6] + "\nDepartmentId=" + dr[7] + "\nAppointmentDate=" + dr[8] + "\nDepartment Name =" + dr[10] + "\nBlood Gropu=" + dr[12]);
+            }
+            Console.WriteLine();
+        }
     }
 
     
